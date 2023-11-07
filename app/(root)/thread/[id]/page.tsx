05c0b7dc9808ddb39f13ee510062d31fffbdf5f6
a7mad1112 <ahmalawneh79@gmail.com>
2023-11-08 00:52:23 +0200

    Render comments

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -1,24 +1,24 @@
-import ThreadCard from "@/components/cards/ThreadCard";
-import { currentUser } from "@clerk/nextjs";
+import ThreadCard from '@/components/cards/ThreadCard';
+import { currentUser } from '@clerk/nextjs';
 import { fetchUser } from '@/lib/actions/user.actions';
 import { redirect } from 'next/navigation';
 import { fetchThreadById } from '@/lib/actions/thread.actions';
 import Comment from '@/components/forms/Comment';
-const Page = async ({ params: {id} }: {params: {id: string}}) => {
-  if(!id) return null;
+const Page = async ({ params: { id } }: { params: { id: string } }) => {
+  if (!id) return null;
   const user = await currentUser();
-  if(!user) return null;
+  if (!user) return null;
   const userInfo = await fetchUser(user.id);
-  if(!userInfo?.onboarded) redirect('/onboarding');
+  if (!userInfo?.onboarded) redirect('/onboarding');
 
   const thread = await fetchThreadById(id);
   return (
     <section className="relative">
       <div>
         <ThreadCard
-          key={thread._id} 
-          id={thread._id} 
-          currentUserId={user?.id || ""}
+          key={thread._id}
+          id={thread._id}
+          currentUserId={user?.id || ''}
           parentId={thread.parentId}
           content={thread.text}
           author={thread.author}
@@ -28,12 +28,29 @@ const Page = async ({ params: {id} }: {params: {id: string}}) => {
         />
       </div>
       <div className="mt-7">
-        <Comment threadId={thread.id}
-          currentUserImg={user.imageUrl}
-          currentUserId={JSON.stringify(userInfo._id)} 
+        <Comment
+          threadId={thread.id}
+          currentUserImg={userInfo.image}
+          currentUserId={JSON.stringify(userInfo._id)}
         />
       </div>
+      <div className="mt-10">
+        {thread.children.map((childrenItem: any) => (
+          <ThreadCard
+            key={childrenItem._id}
+            id={childrenItem._id}
+            currentUserId={user?.id || ''}
+            parentId={childrenItem.parentId}
+            content={childrenItem.text}
+            author={childrenItem.author}
+            community={childrenItem.community}
+            createdAt={childrenItem.createdAt}
+            comments={childrenItem.children}
+            isComment
+          />
+        ))}
+      </div>
     </section>
-  )
-}
+  );
+};
 export default Page;
--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -33,10 +33,14 @@ const ThreadCard = ({
   community,
   createdAt,
   comments,
-  isComment
+  isComment,
 }: Props) => {
   return (
-    <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
+    <article
+      className={`flex w-full flex-col rounded-xl bg-dark-2 p-7 ${
+        isComment ? 'px-0 xs:px-7' : 'bg-dark-2 p-7'
+      }`}
+    >
       <div className="flex items-start justify-between">
         <div className="flex w-full flex-1 flex-row gap-4">
           <div className="flex flex-col items-center">
@@ -90,13 +94,13 @@ const ThreadCard = ({
                   className="cursor-pointer object-contain"
                 />
               </div>
-              {
-                isComment && comments.length > 0 && (
-                  <Link href={`/thread/${id}`}>
-                    <p className="mt-1 text-subtle-medium text-gray-1">{comments.length} replies</p>
-                  </Link>
-                )
-              }
+              {isComment && comments.length > 0 && (
+                <Link href={`/thread/${id}`}>
+                  <p className="mt-1 text-subtle-medium text-gray-1">
+                    {comments.length} replies
+                  </p>
+                </Link>
+              )}
             </div>
           </div>
         </div>
