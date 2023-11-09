e4dfb59130894b5a596e84c8dbfe9cb5464e2843
a7mad1112 <ahmalawneh79@gmail.com>
2023-11-09 19:22:24 +0200

    Render replies inside threadCard

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -102,9 +102,36 @@ const ThreadCard = ({
         {/* TODO: deleteThread */}
         {/* TODO: show comment logos */}
         <p className="text-subtle-meduim text-gray-1">
-          {formatDateString(createdAt)} 
+          {formatDateString(createdAt)}
         </p>
+        {/* <DeleteThread
+          threadId={JSON.stringify(id)}
+          currentUserId={currentUserId}
+          authorId={author.id}
+          parentId={parentId}
+          isComment={isComment}
+        /> */}
       </div>
+      {!isComment && comments.length > 0 && (
+        <div className="ml-1 mt-3 flex items-center gap-2">
+          {comments.slice(0, 2).map((comment, index) => (
+            <Image
+              key={index}
+              src={comment.author.image}
+              alt={`user_${index}`}
+              width={24}
+              height={24}
+              className={`${index !== 0 && '-ml-5'} rounded-full object-cover`}
+            />
+          ))}
+
+          <Link href={`/thread/${id}`}>
+            <p className="mt-1 text-subtle-medium text-gray-1">
+              {comments.length} repl{comments.length > 1 ? 'ies' : 'y'}
+            </p>
+          </Link>
+        </div>
+      )}
     </article>
   );
 };
