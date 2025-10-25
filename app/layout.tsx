6c04a2ce4677dc101cf9533eca5123ec3eb01d85
a7mad1112 <ahmalawneh79@gmail.com>
2025-10-25 21:46:18 +0200

    add publishable key for clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import { ClerkProvider } from '@clerk/nextjs';
-import { dark } from '@clerk/themes';
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
-import './globals.css';
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Threads',
-  description: 'A Next.js 13 Meta Threads application',
+  title: "Threads",
+  description: "A Next.js 13 Meta Threads application",
 };
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
       appearance={{
         baseTheme: dark,
       }}
